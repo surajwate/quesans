b39395cc4f560bd740e83508ed9ecb4863b65a65
Suraj Wate <suraj.wate@gmail.com>
2021-03-15 18:25:37 +0530

    sessions

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
-from flask import Flask, render_template, g, request
+from flask import Flask, render_template, g, request, session, redirect, url_for
+from flask.helpers import url_for
 from database import get_db
 from werkzeug.security import generate_password_hash, check_password_hash
+import os
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.urandom(24)
 
 @app.teardown_appcontext
 def close_db(error):
@@ -11,7 +14,11 @@ def close_db(error):
 
 @app.route('/')
 def index():
-    return render_template('home.html')
+    user = None
+    if 'user' in session:
+        user = session['user']
+
+    return render_template('home.html', user=user)
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -37,6 +44,7 @@ def login():
         user_result = user_cur.fetchone()
 
         if check_password_hash(user_result['password'], password):
+            session['user'] = user_result['name']
             return "true"
         else:
             return "false"
@@ -62,6 +70,11 @@ def unanswered():
 @app.route('/users')
 def users():
     return render_template('users.html')
+
+@app.route('/logout')
+def logout():
+    session.pop('user', None)
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(debug=True)
