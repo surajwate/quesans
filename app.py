2c7aa982aa9b675133f1029ad431db559aecb155
Suraj Wate <suraj.wate@gmail.com>
2021-03-17 16:52:15 +0530

    User setup(user.html) page updated to promote user

--- app.py
+++ app.py
@@ -85,7 +85,17 @@ def unanswered():
 @app.route('/users')
 def users():
     user = get_current_user()
-    return render_template('users.html', user=user)
+    db = get_db()
+    users_cur = db.execute("select id, name, expert, admin from users")
+    users_results = users_cur.fetchall()
+    return render_template('users.html', user=user, all_users=users_results)
+
+@app.route('/promote/<user_id>')
+def promote(user_id):
+    db = get_db()
+    db.execute('update users set expert = 1 where id = ?', [user_id])
+    db.commit()
+    return redirect(url_for('users'))
 
 @app.route('/logout')
 def logout():
