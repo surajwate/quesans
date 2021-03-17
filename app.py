f0da10d81ae7958b253fbd6c65161fade765495d
Suraj Wate <suraj.wate@gmail.com>
2021-03-17 21:06:35 +0530

    answering questions

--- app.py
+++ app.py
@@ -67,10 +67,26 @@ def question():
     user = get_current_user()
     return render_template('question.html', user=user)
 
-@app.route('/answer')
-def answer():
+@app.route('/answer/<question_id>', methods=['POST', 'GET'])
+def answer(question_id):
     user = get_current_user()
-    return render_template('answer.html', user=user)
+    db = get_db()
+
+    if request.method == 'POST':
+        db.execute('''
+                    update 
+                        questions 
+                    set 
+                        answer_text = ? 
+                    where 
+                        id = ?
+        ''', [request.form['answer'], question_id])
+        db.commit()
+        return redirect(url_for('unanswered'))
+
+    ques_cur = db.execute('select id, question_text from questions where id=?', [question_id])
+    question = ques_cur.fetchone()
+    return render_template('answer.html', user=user, question=question)
 
 @app.route('/ask', methods=['GET', 'POST'])
 def ask():
